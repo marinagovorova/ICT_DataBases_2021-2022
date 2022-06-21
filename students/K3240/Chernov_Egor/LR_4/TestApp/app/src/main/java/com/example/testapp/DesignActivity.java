10a033b7b6fcbe26ffbe82a70a3a19c17c758bdf
EgorChernov5 <EgorChernov3140@gmail.com>
2022-06-21 18:06:44 +0300

    Был добавлен проект для 4 лабораторной работы

---
+++ students/K3240/Chernov_Egor/LR_4/TestApp/app/src/main/java/com/example/testapp/DesignActivity.java
@@ -0,0 +1,14 @@
+package com.example.testapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DesignActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_design);
+    }
+}
